96c7d293b32e6ed63906e0c73bed84b544020f31
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2023-01-27 10:34:18 +0300

    S5G4 Completed

--- ip/index.js
+++ ip/index.js
@@ -40,17 +40,17 @@ async function ipAdresimiAl() {
     DOM metotlarını ve özelliklerini kullanarak, şunları gerçekleştirin:
 	
 	<div class="card">
-	<img src={ülke bayrağı url} />
-	<div class="card-info">
-		<h3 class="ip">{ip adresi}</h3>
-		<p class="ulke">{ülke bilgisi (ülke kodu)}</p>
-		<p>Enlem: {enlem} Boylam: {boylam}</p>
-		<p>Şehir: {şehir}</p>
-		<p>Saat dilimi: {saat dilimi}</p>
-		<p>Para birimi: {para birimi}</p>
-		<p>ISP: {isp}</p>
-	</div>
+    <img src={ülke bayrağı url} />
+    <div class="card-info">
+      <h3 class="ip">{ip adresi}</h3>
+      <p class="ulke">{ülke bilgisi (ülke kodu)}</p>
+      <p>Enlem: {enlem} Boylam: {boylam}</p>
+      <p>Şehir: {şehir}</p>
+      <p>Saat dilimi: {saat dilimi}</p>
+      <p>Para birimi: {para birimi}</p>
+      <p>ISP: {isp}</p>
     </div>
+  </div>
 */
 
 /*
@@ -65,7 +65,25 @@ async function ipAdresimiAl() {
 	bilgisayarınızın IP adresini atayacaktır. 
 	Örnek dinamik URL kullanımı: var url = "https://apis.ergineer.com/ipgeoapi/"+benimIP; 
 */
-
+/* const dummy = {
+  sorgu: "88.236.96.168",
+  durum: "OK",
+  kıta: "Asia",
+  ülke: "Turkey",
+  ülkeKodu: "TR",
+  ülkebayrağı: "https://apis.ergineer.com/ulkebayraklari/TR",
+  bölge: "25",
+  bölgeAdı: "Erzurum",
+  şehir: "Erzurum",
+  zip: "09818",
+  enlem: 39.9055000000000035242919693700969219207763671875,
+  boylam: 41.2657999999999987039700499735772609710693359375,
+  saatdilimi: "Europe/Istanbul",
+  parabirimi: "TRY",
+  isp: "TurkTelecom",
+  organizasyon: "Turk Telekomunikasyon A.S",
+  as: "AS47331 TTNet A.S.",
+}; */
 //kodlar buraya gelecek
 let cards = document.querySelector(".cards");
 function cardAll(nesne) {
@@ -73,7 +91,7 @@ function cardAll(nesne) {
   cardDiv.classList = "card";
 
   let cardImg = document.createElement("img");
-  cardImg.src = "{ülke bayrağı url}";
+  cardImg.src = nesne.ülkebayrağı;
   cardDiv.appendChild(cardImg);
 
   let cardDiv1 = document.createElement("div");
@@ -82,35 +100,49 @@ function cardAll(nesne) {
 
   let cardH3 = document.createElement("h3");
   cardH3.classList = "ip";
-  cardH3.textContent = "{ip adresi}";
-  cardDiv.appendChild(cardH3);
+  cardH3.textContent = nesne.sorgu;
+  cardDiv1.appendChild(cardH3);
 
   let cardP = document.createElement("p");
   cardP.classList = "ulke";
-  cardP.textContent = "{ülke bilgisi (ülke kodu)}";
-  cardDiv.appendChild(cardP);
+  cardP.textContent = `${nesne.ülke} (${nesne.ülkeKodu})`;
+  cardDiv1.appendChild(cardP);
 
   let cardP1 = document.createElement("p");
-  cardP1.textContent = "{enlem} Boylam: {boylam}";
-  cardDiv.appendChild(cardP1);
+  cardP1.textContent = `Enlem: ${nesne.enlem} Boylam: ${nesne.boylam}`;
+  cardDiv1.appendChild(cardP1);
 
   let cardP2 = document.createElement("p");
-  cardP2.textContent = "Şehir: {şehir}";
-  cardDiv.appendChild(cardP2);
+  cardP2.textContent = `Şehir: ${nesne.şehir}`;
+  cardDiv1.appendChild(cardP2);
 
   let cardP3 = document.createElement("p");
-  cardP3.textContent = "Saat dilimi: {saat dilimi}";
-  cardDiv.appendChild(cardP3);
+  cardP3.textContent = `Saat dilimi: ${nesne.saatdilimi}`;
+  cardDiv1.appendChild(cardP3);
 
   let cardP4 = document.createElement("p");
-  cardP4.textContent = "Para birimi: {para birimi}";
-  cardDiv.appendChild(cardP4);
+  cardP4.textContent = `Para birimi: ${nesne.parabirimi}`;
+  cardDiv1.appendChild(cardP4);
 
   let cardP5 = document.createElement("p");
-  cardP5.textContent = "ISP: {isp}";
-  cardDiv.appendChild(cardP5);
+  cardP5.textContent = ` ISP: ${nesne.isp}`;
+  cardDiv1.appendChild(cardP5);
 
   return cardDiv;
 }
 
-cards.appendChild(cardAll(benimIP));
+const connection = async function () {
+  await ipAdresimiAl();
+  await axios({
+    method: "get",
+    url: "https://apis.ergineer.com/ipgeoapi/" + benimIP,
+  })
+    .then(function (response) {
+      return response.data;
+    })
+    .then(function (a) {
+      cards.appendChild(cardAll(a));
+    });
+};
+
+connection();
